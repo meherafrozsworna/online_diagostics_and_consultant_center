9634239bac6102b8cc6ea17fc27ebf43650f91bb
Umama092 <83491898+Umama092@users.noreply.github.com>
2021-05-27 12:42:26 +0600

    Update App.js

--- fontend/src/App.js
+++ fontend/src/App.js
@@ -1,24 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { BrowserRouter, Route } from 'react-router-dom';
+import HomeScreen from './screens/HomeScreen';
+import AboutScreen from './screens/AboutScreen';
+import DoctorScreen from './screens/DoctorScreen';
+import SigninScreen from './screens/SigninScreen';
+import RegisterScreen from './screens/RegisterScreen';
+import TestFormScreen from './screens/TestFormScreen';
+import PatientHomeScreen from './screens/PatientHomeScreen';
+
 
 function App() {
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+
+  <Route path="/about" component={AboutScreen}></Route>
+  <Route path="/signin" component={SigninScreen}></Route>
+  <Route path="/register" component={RegisterScreen}></Route>
+  <Route path="/patienthome" component={PatientHomeScreen}></Route>
+  <Route path="/testform" component={TestFormScreen}></Route>
+  <Route path="/" component={HomeScreen} exact></Route>
+
+    
+    
+    </BrowserRouter>
   );
 }
 
