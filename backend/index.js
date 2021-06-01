4b13fc72def2d4de4330ba715ae3521c395aa875
AdibaShaira <40664860+AdibaShaira@users.noreply.github.com>
2021-06-02 05:02:27 +0700

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -1,5 +1,5 @@
 const express = require('express');
-//const cors = require('cors');
+const cors = require('cors');
 const mongoose = require('mongoose');
 
 // require('dotenv').config();
@@ -7,7 +7,7 @@ const mongoose = require('mongoose');
 const app = express();
 const port = process.env.PORT || 5000;
 
-//app.use(cors());
+app.use(cors());
 app.use(express.json());
 
 const uri = "mongodb://localhost:27017/diagnostics";
