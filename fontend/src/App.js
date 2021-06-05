0697fcc85c5ccaa25b95adac12676e90869889ef
Umama092 <83491898+Umama092@users.noreply.github.com>
2021-06-05 16:11:03 +0600

    Update App.js

--- fontend/src/App.js
+++ fontend/src/App.js
@@ -3,9 +3,11 @@ import { BrowserRouter, Route } from 'react-router-dom';
 import HomeScreen from './screens/HomeScreen';
 import AboutScreen from './screens/AboutScreen';
 import DoctorScreen from './screens/DoctorScreen';
+import TestForm from './screens/TestForm';
+import SampleCollector from './screens/SampleCollector';
+import ReportPrescription from './screens/ReportPrescription';
 import SigninScreen from './screens/SigninScreen';
 import RegisterScreen from './screens/RegisterScreen';
-import TestFormScreen from './screens/TestFormScreen';
 import PatientHomeScreen from './screens/PatientHomeScreen';
 
 function App() {
@@ -15,6 +17,7 @@ function App() {
             <Route path="/about" component={AboutScreen}></Route>
             <Route path="/signin" component={SigninScreen}></Route>
             <Route path="/register" component={RegisterScreen}></Route>
+            <Route path="/samplecollector" component={SampleCollector}></Route>
             <Route
                 path="/patienthome/:id"
                 
@@ -26,7 +29,9 @@ function App() {
                   /*using render to send custom props*/
                 } >
             </Route>
-            <Route path="/testform" component={TestFormScreen}></Route>
+            <Route path="/testform" component={TestForm}></Route>
+            <Route path="/reportpres" component={ReportPrescription}></Route>
+            <Route path="/doctor" component={DoctorScreen}></Route>
             <Route path="/" component={HomeScreen} exact></Route>
         </BrowserRouter>
     );
