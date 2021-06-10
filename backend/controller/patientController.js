aaf7f5fbf69f8a2a8187c268bfe6a6095d031b36
meherafrozsworna <meherafroz6331@gmail.com>
2021-06-10 21:17:59 +0600

    jwttoken

--- backend/controller/patientController.js
+++ backend/controller/patientController.js
@@ -2,99 +2,137 @@
 //const config = require('config');
 const bcrypt = require('bcrypt');
 //const _ = require('lodash');
-const Patient = require("../model/patient");
+const Patient = require('../model/patient');
 //const {Patient, validate} = require('../model/patient');
 //const Joi = require('@hapi/joi');
 const mongoose = require('mongoose');
 const express = require('express');
+const jwt = require('jsonwebtoken');
 const router = express.Router();
-router.get("/",(req,res)=>{
-    console.log("ashche");
- res.send("HomePage of Helathway");
+
+const verifyJWT = (req, res, next) => {
+    const token = req.headers['x-access-token'];
+
+    if (!token) {
+        res.send('We need a token');
+    } else {
+        jwt.verify(token, 'jwtSecrete', (err, decoded) => {
+            if (err) {
+                res.send({
+                    auth: false,
+                    message: 'you failed to authenticate',
+                });
+            } else {
+                req.patient = decoded.patient;
+                next();
+            }
+        });
+    }
+};
+
+router.get('/isUserAuth', verifyJWT, (req, res) => {
+    res.send('You are authenticated......<3');
 });
-router.post("/add", async(req, res) => {   
-    console.log("working 1") 
+
+router.get('/', (req, res) => {
+    console.log('ashche');
+    res.send('HomePage of Helathway');
+});
+router.post('/add', async (req, res) => {
+    console.log('working 1');
     let patient = new Patient();
     patient.name = req.body.name;
     patient.email = req.body.email;
     const salt = await bcrypt.genSalt(10);
     patient.password = await bcrypt.hash(req.body.password, salt);
     //patient.password = req.body.password;
-    console.log("working 2") 
-    patient.age=req.body.age;
+    console.log('working 2');
+    patient.age = req.body.age;
     patient.address = req.body.address;
     patient.phone = req.body.phone;
     patient.gender = req.body.gender;
     patient.bloodGroup = req.body.bloodGroup;
-   // patient.historyId=null;
-    patient.save(err => {
+    // patient.historyId=null;
+    patient.save((err) => {
         if (err) return res.json({ success: false, error: err });
         return res.json({ success: true });
     });
-
 });
 
-router.post("/login", async(req, res) => {   
+router.post('/login', async (req, res) => {
     //validate will be different
-      //  const { error } = validatePatient(req.body); 
-      //  if (error) return res.status(400).send(error.details[0].message);
-        let patient = await Patient.findOne({ email: req.body.email });
-        if (!patient) return res.status(400).send('Invalid Email');
-        console.log(`Password ${req.body.password}`);
-        console.log(`Password ${patient.password}`);
-        const validPassword = await bcrypt.compare(req.body.password, patient.password);
-        if (validPassword) {
-            res.send(patient);
-          } else {
-            return res.status(400).send('Invalid password.');
-          }
-        
-         //   console.log(`Password ${patient.password}`);
-        //console.log(`Password ${req.body.password}`);
-        //if(req.body.password==patient.password){
-        //    res.send(patient);
-        //}
-        //else{
-        //    return res.status(400).send('Invalid password.');
-        //}
-    
+    //  const { error } = validatePatient(req.body);
+    //  if (error) return res.status(400).send(error.details[0].message);
+    let patient = await Patient.findOne({ email: req.body.email });
+    if (!patient)
+        return res.status(400).send({ auth: false, message: 'Invalid Email' });
+    console.log(`Password ${req.body.password}`);
+    console.log(`Password ${patient.password}`);
+    const validPassword = await bcrypt.compare(
+        req.body.password,
+        patient.password
+    );
+    if (validPassword) {
+        const token = jwt.sign({ patient }, 'jwtSecrete', {
+            expiresIn: 300,
+        });
+        res.send({ auth: true, token: token, result: patient });
+    } else {
+        return res.status(400).send({
+            auth: false,
+            message: 'wrong username/password combination',
+        });
+    }
+
+    //   console.log(`Password ${patient.password}`);
+    //console.log(`Password ${req.body.password}`);
+    //if(req.body.password==patient.password){
+    //    res.send(patient);
+    //}
+    //else{
+    //    return res.status(400).send('Invalid password.');
+    //}
 });
 
 //eta add korsi patient er edit er jonne
-router.put("/:id/edit",async (req,res)=>{
-    
-    const patient = await Patient.findByIdAndUpdate(req.params.id,
-        { 
-            name: req.body.name ,
-            email :req.body.email,
-            password : req.body.password,
-            age:req.body.age,
-            address : req.body.address,
-            phone : req.body.phone,
-            gender : req.body.gender,
-            bloodGroup :req.body.bloodGroup,
-            historyId:req.body.historyId
-        }, { new: true });
-        if (!patient) return res.status(404).send('The patient with the given ID was not found.');
-  
-        res.send(patient);
-}
-);
+router.put('/:id/edit', async (req, res) => {
+    const patient = await Patient.findByIdAndUpdate(
+        req.params.id,
+        {
+            name: req.body.name,
+            email: req.body.email,
+            password: req.body.password,
+            age: req.body.age,
+            address: req.body.address,
+            phone: req.body.phone,
+            gender: req.body.gender,
+            bloodGroup: req.body.bloodGroup,
+            historyId: req.body.historyId,
+        },
+        { new: true }
+    );
+    if (!patient)
+        return res
+            .status(404)
+            .send('The patient with the given ID was not found.');
+
+    res.send(patient);
+});
 
 //egula amader na
-router.get("/patients/:id/admit", function (req, res) {
+router.get('/patients/:id/admit', function (req, res) {
     const id = { id: req.params.id };
     const update = {
         admitted: true,
-        dateAdmitted: Date.now()
+        dateAdmitted: Date.now(),
     };
     Patient.findOneAndUpdate(id, update, function (err, patient) {
         if (err) return res.json({ success: false, error: err });
         return res.json(patient);
     });
 });
-router.get('/:id', async (req, res) => {
-    const patient = await Doctor.findById(req.params.id);
+router.get('/:id', verifyJWT, async (req, res) => {
+    const patient = await Patient.findById(req.params.id);
     if (!patient)
         return res
             .status(404)
@@ -103,7 +141,7 @@ router.get('/:id', async (req, res) => {
     res.send(patient);
 });
 //egula amader na
-router.get("/patients/:id/discharge", function (req, res) {
+router.get('/patients/:id/discharge', function (req, res) {
     const id = { id: req.params.id };
     const update = {
         admitted: false,
@@ -119,18 +157,17 @@ router.get("/patients/:id/discharge", function (req, res) {
 //eta validation er jonne add korsilam
 function validatePatient(patient) {
     const schema = {
-      name: Joi.string().min(3).required(),
-      email:Joi.string().required(),
-      password:Joi.string().required(),
-      age:Joi.number().required(),
-      address:Joi.string().required(),
-      phone:Joi.number().required(),
-      gender:Joi.string().required(),
-      bloodGroup:Joi.string().required()
-
+        name: Joi.string().min(3).required(),
+        email: Joi.string().required(),
+        password: Joi.string().required(),
+        age: Joi.number().required(),
+        address: Joi.string().required(),
+        phone: Joi.number().required(),
+        gender: Joi.string().required(),
+        bloodGroup: Joi.string().required(),
     };
-  
+
     return Joi.validate(patient, schema);
-  }
+}
 
 module.exports = router;
--- fontend/src/screens/HomeScreen.js
+++ fontend/src/screens/HomeScreen.js
@@ -3,45 +3,34 @@ import { Link } from 'react-router-dom';
 
 export default function HomeScreen() {
     return (
-
         <div className="homescreen">
-      <header className="row">
-        <div>
-          <Link className="brand" href="/">Healthway</Link>
+            <header className="row">
+                <div>
+                    <Link className="brand" to="/">
+                        Healthway
+                    </Link>
+                </div>
+                <div>
+                    <Link to="/about">About</Link> |
+                    <Link to="/patienthome">Services</Link> |
+                    {<Link to="/signin">Sign In </Link>}
+                </div>
+            </header>
+            <main></main>
+            <footer>
+                <div className="row center">
+                    <div>
+                        <div className="loginbuttons">
+                            <Link to="/signin" className="btn">
+                                Sign In
+                            </Link>
+                            <Link to="/register" className="btn">
+                                Register
+                            </Link>
+                        </div>
+                    </div>
+                </div>
+            </footer>
         </div>
-        <div>
-          <Link to="/about">About</Link>   |  
-          <Link to="/patienthome">Services</Link>   | 
-
-          {
-              <Link to="/signin" >Sign In         </Link>  
-          }
-                  
-        </div>
-      </header>
-      <main>
-
-     
-        </main>
-      <footer>
-      <div className="row center">
-<div >
-
-  <div className="loginbuttons">
-
-   <Link to="/signin" className="btn">Sign In</Link>
-   <Link to="/register" className="btn">Register</Link>
-
-  </div>
-
-</div>
-</div>
-
-
-      </footer>
-    </div>
-
-
     );
-
 }
