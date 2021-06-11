8f1c271d396b7200d225fbbd36e7918a29a54aea
Umama092 <83491898+Umama092@users.noreply.github.com>
2021-06-12 01:54:17 +0600

    Update App.js

--- admin/src/App.js
+++ admin/src/App.js
@@ -4,6 +4,10 @@ import HomeScreen from './screens/HomeScreen';
 import AdminHome from './screens/AdminHome';
 import AdminTest from './screens/AdminTestApprove';
 import AdminSignin from './screens/AdminSignin';
+import addCollector from './screens/addCollector';
+import registerCollector from './screens/registerCollector';
+import addDoctor from './screens/addDoctor';
+import registerDoctor from './screens/registerDoctor';
 
 
 function App() {
@@ -15,6 +19,10 @@ function App() {
             <Route path="/signinadmin" component={AdminSignin}></Route>  
             <Route path="/adminhome" component={AdminHome}></Route>
             <Route path="/admintest" component={AdminTest}></Route>          
+            <Route path="/addcollector" component={addCollector}></Route>  
+            <Route path="/registercollector" component={registerCollector}></Route>    
+            <Route path="/adddoctor" component={addDoctor}></Route>  
+            <Route path="/registerdoctor" component={registerDoctor}></Route>  
 
         </BrowserRouter>
     );
