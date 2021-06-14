1e44a4c634a756349d1d85020d52346db5e8de9c
meherafrozsworna <meherafroz6331@gmail.com>
2021-06-14 20:38:10 +0600

    register

--- backend/index.js
+++ backend/index.js
@@ -10,25 +10,26 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-const uri = "mongodb://localhost:27017/diagnostics";
-mongoose.connect(uri, { useNewUrlParser: true, useCreateIndex: true }
-);
+const uri = 'mongodb://localhost:27017/diagnostics';
+mongoose.connect(uri, { useNewUrlParser: true, useCreateIndex: true });
 const connection = mongoose.connection;
 connection.once('open', () => {
-  console.log("MongoDB database connection established successfully");
-})
+    console.log('MongoDB database connection established successfully');
+});
 
 const patientRouter = require('./controller/patientController');
 const doctorRouter = require('./controller/doctorController');
-const historyRouter=require('./controller/historyController');
-const reportRouter=require('./controller/reportController');
-const sampleCollectorRouter=require('./controller/sampleCollectorController');
+const historyRouter = require('./controller/historyController');
+const reportRouter = require('./controller/reportController');
+const sampleCollectorRouter = require('./controller/sampleCollectorController');
+const adminRouter = require('./controller/adminController');
 
+app.use('/admin', adminRouter);
 app.use('/patient', patientRouter);
-app.use('/doctor', doctorRouter) ;
-app.use('/history',historyRouter);
-app.use('/report',reportRouter);
-app.use('/sampleCollector',sampleCollectorRouter);
+app.use('/doctor', doctorRouter);
+app.use('/history', historyRouter);
+app.use('/report', reportRouter);
+app.use('/sampleCollector', sampleCollectorRouter);
 app.listen(port, () => {
     console.log(`Server is running on port: ${port}`);
 });
