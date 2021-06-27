ddb73958334d7a3a21d32ba025887fb0b5b7c70e
adiba <adibashairarifa@gmail.com>
2021-06-28 02:11:21 +1200

    files

--- backend/controller/reportController.js
+++ backend/controller/reportController.js
@@ -3,22 +3,43 @@ const Joi = require('@hapi/joi');
 const mongoose = require('mongoose');
 const express = require('express');
 const router = express.Router();
+var multer = require('multer');
 router.get("/",(req,res)=>{
  res.send("HomePage of Helathway");
 });
-router.post("/addReport", async(req, res) =>{
+var storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+    cb(null, 'public')
+  },
+  filename: function (req, file, cb) {
+    cb(null, Date.now() + '-' +file.originalname )
+  }
+})
+var upload = multer({ storage: storage }).single('file');
+router.post("/addReport",upload,async(req, res) =>{
     report = new Report({
+        id:req.body.id,
         name:req.body.name,
         age:req.body.age,
-        phone:req.body.phone,
+        phone:req.body.phone, 
         date:req.body.date,
         testName:req.body.testName,
         details:req.body.details
         });
+        if(req.file){
+            report.fileStorage=req.file.path;
+        }
         report.save(err => {
             if (err) return res.json({ success: false, error: err });
             return res.json({ success: true });
         });
+
+});
+router.get('/:id/filelocation',async(req, res) =>{
+    let report = await Report.find({
+        id: req.params.id,
+    });
+    res.json(report);
 
 });
 router.put("/:id/edit", async(req, res) =>{
@@ -37,4 +58,6 @@ router.put("/:id/edit", async(req, res) =>{
     res.send(report);
 
 });
+
+
 module.exports = router;
