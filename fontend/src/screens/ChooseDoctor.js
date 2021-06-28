fdca0c343c1e58038aa01adb5310f8608e1b6daa
Umama092 <83491898+Umama092@users.noreply.github.com>
2021-06-28 13:27:51 +0600

    Update ChooseDoctor.js

--- fontend/src/screens/ChooseDoctor.js
+++ fontend/src/screens/ChooseDoctor.js
@@ -3,38 +3,43 @@ import { Link } from 'react-router-dom';
 import axios from 'axios';
 
 //export default function PatientHomeScreen() {
-export default class SampleCollector extends Component {
+export default class ChooseDoctor extends Component {
 
-   
+
     render() {
-        //const data = localStorage.getItem('data');
         return (
 
-            
+
             <div className="profile2">
                 {
-                    
+
                 }
 
                 <header className="row">
                     <div>
-                        <a className="brand" href="/">
+                        <a className="brand" href="/patienthome/:id">
                             Home
-                        </a>
-                    </div>
-                    <div className="row center">
-                        <a className="brand2" href="/signin">
-                            {' '}
-                            Sample Collection |{' '}
-                        </a>
-                        <a className="brand2" href="/signin">
-                            {' '}
-                            Feedback{' '}
                         </a>
                     </div>
                 </header>
                 <main className="profile">
-                    
+
+
+                    <div class="card">
+                    <img src="http://hellojivan.com/assets/websites/assets/img/doctors/doctor-thumb-02.jpg" alt="Dr_Reza" style={{width:"100%"}}/> 
+                        <h3>Dr Rezaul Karim</h3>
+                        <p class="title">
+                            Assisstant Professor, DMC <br></br>
+                            MBBS, DTCH (DMC) 
+                        </p>
+
+                        <p> <h2>
+                            Sun-Thurs: 6pm-9pm
+                        </h2></p>
+                       
+                        <Link to="/signin" className="book_btn">Book Appointment</Link>
+                    </div>
+
                 </main>
             </div>
         );
