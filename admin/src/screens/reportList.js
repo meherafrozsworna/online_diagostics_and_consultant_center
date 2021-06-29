07f34e81e463a1170b2abc859f583dee7a764705
adiba <adibashairarifa@gmail.com>
2021-06-29 21:40:11 +1200

    no

--- admin/src/screens/reportList.js
+++ admin/src/screens/reportList.js
@@ -45,6 +45,8 @@ export default class ReportList extends Component {
     render() {
         const listItems = this.state.reportList.map((d) => (
             //   "/fileupload/:formid/patient/:patientid"
+            console.log('kkkkkk'),
+            console.log(d.patientId),
             <li>
                 <Link to={'/fileupload/' + d._id + '/patient/' + d.patientId}>
                     {
