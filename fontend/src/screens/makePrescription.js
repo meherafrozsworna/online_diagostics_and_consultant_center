47de7ffd454a672d997f6ae84d93ac493c343224
Umama092 <83491898+Umama092@users.noreply.github.com>
2021-06-29 23:01:04 +0600

    logout

--- fontend/src/screens/makePrescription.js
+++ fontend/src/screens/makePrescription.js
@@ -131,6 +131,9 @@ export default class TestForm extends Component {
                             Home
                         </a>
                     </div>
+                     <div>
+                        <Link to="/">Log Out{'  '}</Link>
+                    </div>
                 </header>
 
                 <main className="testform">
